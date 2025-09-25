b4a8c90cdffddbb5b79738791160ec427cb84cdb
sthefany-bn <sthy1917@gmail.com>
2025-09-25 11:08:46 -0300

    detalhes

--- chamado/utils/decorators.py
+++ chamado/utils/decorators.py
@@ -4,7 +4,7 @@ from django.core.exceptions import PermissionDenied
 def adm_required(view_func):
     @login_required(login_url='/usuario/login/')
     def proibido(request, *args, **kwargs):
-        if not getattr(request.user, 'adm', False):
+        if not request.user.perfil.adm:
             raise PermissionDenied
         return view_func(request, *args, **kwargs)
     return proibido
